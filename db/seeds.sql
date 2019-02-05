9dcd3b5b17a7f39e62afda3e68d063d7ba45d9bf
lupitaleyva <mgleya16@gmail.com>
2019-02-05 17:30:51 -0600

    updating

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Mc and Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Blue Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Big Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Ranch Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Guacamole Burger', true);
+INSERT INTO burgers (name) VALUES ('Barbaque Burger');
